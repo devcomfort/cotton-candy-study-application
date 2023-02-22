e94d6a9a2d1fb19a87adca0b49cab231d2bf2d78
hmyo2853 <hmyo2853@gmail.com>
2023-02-22 21:20:31 +0900

    :lipstick: edit style

--- src/components/Logged.tsx
+++ src/components/Logged.tsx
@@ -36,18 +36,24 @@ const Logged = (props: StorageType) => {
   const handleModalShowBtn = () => setActivityModal(true);
 
   return (
-    <LoginWrap>
+    <>
       {activityModal && <InviteModal />}
       <LobbyImg />
       <LoginInfoWrapper>
         <LoginUserName>{userName}님, 환영합니다.</LoginUserName>
         <LoginCreateJoinWrapper>
-          <LoginCreateRoom onClick={pathCreateRoom}>새로운 스터디룸 생성하기</LoginCreateRoom>
-          <LoginEnterRoomIn onClick={handleModalShowBtn}>친구들의 스터디룸 입장하기 (초대코드)</LoginEnterRoomIn>
-          <LoginSearchFeedBack onClick={pathFeedBack}>피드백 검색하기</LoginSearchFeedBack>
+          <LoginCreateRoom onClick={pathCreateRoom}>
+            새로운 스터디룸 생성하기
+          </LoginCreateRoom>
+          <LoginEnterRoomIn onClick={handleModalShowBtn}>
+            친구들의 스터디룸 입장하기 (초대코드)
+          </LoginEnterRoomIn>
+          <LoginSearchFeedBack onClick={pathFeedBack}>
+            피드백 검색하기
+          </LoginSearchFeedBack>
         </LoginCreateJoinWrapper>
       </LoginInfoWrapper>
-    </LoginWrap>
+    </>
   );
 };
 
