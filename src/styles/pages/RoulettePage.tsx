e2eb0331ef4833d48234de3479fd7343bb9d6cee
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-02-26 19:56:11 +0900

    :lipstick: height 디자인 이슈 수정

--- src/styles/pages/RoulettePage.tsx
+++ src/styles/pages/RoulettePage.tsx
@@ -2,7 +2,7 @@ import styled from "@emotion/styled";
 
 export const RouletteWrapper = styled.div`
   width: 100%;
-  height: 100vh;
+  padding-top: 100px;
   display: flex;
   flex-direction: column;
   justify-content: center;
