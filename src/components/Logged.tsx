51c93df072486fdd7a57a3e174c23b33714241c4
LK1 <64725837+LAC-LK1@users.noreply.github.com>
2023-03-02 01:21:56 +0900

    :recycle: Logged Component SourceCode Refactoring

--- src/components/Logged.tsx
+++ src/components/Logged.tsx
@@ -38,18 +38,20 @@ const Logged = (props: StorageType) => {
   // URl 경로 /feedback으로 이동하는 함수
   const pathFeedBack = () => path("/feedback");
 
-  /** 스터디룸 입장모달 활성화 */
-  const handleModalShowBtn = () => setActivityModal(true);
+  /** 스터디룸 입장모달 활성화
+   * state의 기존값을 반대로 저장하며, InviteModal 컴포넌트 props로 전달해준다.
+   */
+  const handleActivityModalBtn = () => setActivityModal((prev) => !prev);
 
   return (
     <>
-      {activityModal && <InviteModal />}
+      {activityModal && <InviteModal handleActivityModalBtn={handleActivityModalBtn} />}
       <LobbyImg />
       <LoginInfoWrapper>
         <LoginUserName>{userName}님, 환영합니다.</LoginUserName>
         <LoginCreateJoinWrapper>
           <LoginCreateRoom onClick={pathCreateRoom}>새로운 스터디룸 생성하기</LoginCreateRoom>
-          <LoginEnterRoomIn onClick={handleModalShowBtn}>친구들의 스터디룸 입장하기 (초대코드)</LoginEnterRoomIn>
+          <LoginEnterRoomIn onClick={handleActivityModalBtn}>친구들의 스터디룸 입장하기 (초대코드)</LoginEnterRoomIn>
           <LoginSearchFeedBack onClick={pathFeedBack}>피드백 검색하기</LoginSearchFeedBack>
         </LoginCreateJoinWrapper>
       </LoginInfoWrapper>
