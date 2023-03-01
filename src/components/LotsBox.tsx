a0560ec06511f4962b6061ea20bdcddcb7520c09
hmyo2853 <hmyo2853@gmail.com>
2023-03-01 21:43:09 +0900

    :sparkles: userDataArr 파라미터를 받아서 array로 반환하는 값 설정

--- src/components/LotsBox.tsx
+++ src/components/LotsBox.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Confetti from "react-confetti";
 import {
   LotsBoxWrap,
@@ -7,55 +7,65 @@ import {
   ShakingLotsBox,
 } from "../styles/components/LotsBox";
 
-const LotsBox = () => {
-  // dummy data
-  const dummyUserArray = [
-    { name: "1. 솜사탕" },
-    { name: "2. 데브컴포트" },
-    { name: "3. 준서킴" },
-    { name: "4. 혜린" },
-    { name: "5. 비니루" },
-  ];
+interface UserType {
+  userDataArr: string[];
+}
 
+const LotsBox = ({ userDataArr }: UserType) => {
+  // Box의 흔들림을 체크하는 state
   const [isShakeBox, setIsShakeBox] = useState(true);
-  const [isShakeConfetti, setIsShakeConfetti] = useState(false);
-  const [userData, setUserData] = useState(dummyUserArray);
+  // 뽑히는 user의 인덱스 state
   const [userIdx, setUserIdx] = useState(0);
-  const [pickupData, setPickupData] = useState<Array<string>>([]);
+  // Confetti(폭죽) 체크하는 state
+  const [isShakeConfetti, setIsShakeConfetti] = useState(false);
+  // props로 전달된 userDataArr을 state로 저장
+  const [userData, setUserData] = useState(userDataArr);
+  // 뽑힌 사람들이 역순으로 들어간 배열 state
+  const [pickupData, setPickupData] = useState<string[]>([]);
 
+  // 박스가 흔들릴때의 함수
   const onShakeBox = () => {
+    // 박스 흔들림 state -> true로 변경
     setIsShakeBox(true);
+    // Confetti 터짐 state -> true로 변경
     setIsShakeConfetti(true);
+    // 2초 뒤에 userDataArr에서 뽑힌 유저 삭제 함수 실행
     setTimeout(() => {
       removeUserData();
     }, 2000);
   };
 
-  // remove user data
+  // userDataArr에서 뽑힌 유저 삭제하는 함수
   const removeUserData = () => {
+    // userLength로 관련 로직 계산
     const userLength = userData.length - 1;
+    // 뽑혔을때 Confetti 터짐 state -> false로 변경
     setIsShakeConfetti(false);
+    // 뽑혔을때 박스 흔들림 state -> false로 변경
     setIsShakeBox(false);
+    // userDataArr의 길이만큼 랜덤으로 인덱스 값 설정
     setUserIdx(Math.floor(Math.random() * userLength));
-    alert(`뽑힌 사람 : ${userData[userIdx].name}`);
+    // 뽑힌사람 알려주기
+    alert(`뽑힌 사람 : ${userData[userIdx]}`);
+
+    // userDataArr에 1명 남았을 때
     if (userLength === 0) {
-      // 모두 뽑혔을때의 로직 추가
-      alert(`모두 뽑힘, 마지막 ${userData[userLength].name}`);
-      setPickupData([userData[userIdx].name, ...pickupData]);
+      // pickupData에 마지막 남은 1명 추가
+      setPickupData([userData[0], ...pickupData]);
+      // userDataArra에 해당 인덱스 1명 제외
       userData.splice(userIdx, 1);
+      // 제외한 데이터 저장
       setUserData([...userData]);
-      // 모두 뽑혔을때 버튼 더이상 동작하지 않도록 삭제
-      console.log(pickupData);
       return;
     }
-    setPickupData([userData[userIdx].name, ...pickupData]);
+    // 그 외
+    // userDataArr[랜덤 인덱스] 유저값을 pickupData에 저장
+    setPickupData([userData[userIdx], ...pickupData]);
+    // userDataArra에 해당 인덱스 1명 제외
     userData.splice(userIdx, 1);
+    // 제외한 데이터 저장
     setUserData([...userData]);
   };
-
-  useEffect(() => {
-    // 데이터로 가져옴
-  }, []);
 
   return (
     <LotsBoxWrap>
@@ -71,7 +81,7 @@ const LotsBox = () => {
       {pickupData.map((item, i) => (
         <span key={i}>{item}</span>
       ))}
-      {/* 다 뽑히고 1명만 남는다면 피드백 모달창 출력 */}
+      {/* @솜사탕 작업할 곳 :: 다 뽑히고나면 피드백 모달창 출력 */}
       {userData.length === 0 ? <></> : null}
     </LotsBoxWrap>
   );
